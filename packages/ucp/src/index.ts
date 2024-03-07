3bbdcc7932e4719d8aabe1fbbde5b5ea427279cc
woutslabbinck <wout.slabbinck@ugent.be>
2024-03-07 10:04:59 +0100

    fix: add exports and change flow file so it works again

--- packages/ucp/src/index.ts
+++ packages/ucp/src/index.ts
@@ -8,6 +8,10 @@ export * from './storage/DirectoryUCRulesStorage'
 export * from './storage/MemoryUCRulesStorage'
 export * from './storage/UCRulesStorage'
 
+// policy
+export * from './policy/ODRL'
+export * from './policy/UsageControlPolicy'
+
 // util
 export * from './util/Conversion'
 export * from './util/Constants'
