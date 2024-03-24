5e16da9382d1b29f9797b9710f8b5a4b7daa9401
Wouter Termont <wouter.termont@ugent.be>
2024-03-24 15:58:05 +0100

    fix: correct resource description typing
    Signed-off-by: Wouter Termont <wouter.termont@ugent.be>

--- packages/uma/src/views/ResourceDescription.ts
+++ packages/uma/src/views/ResourceDescription.ts
@@ -1,12 +1,12 @@
 import { Type, array, optional as $, string } from "../util/ReType";
-import { ScopeDescription } from "./ScopeDescription.js";
+import { ScopeDescription } from "./ScopeDescription";
 
 export const ResourceDescription = {
   resource_scopes: array(string),
   type: $(string),
   name: $(string),
   icon_uri: $(string),
-  description: $(ScopeDescription),
+  description: $(string),
 };
 
 export type ResourceDescription = Type<typeof ResourceDescription>;
