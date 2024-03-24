81ffbc46f6731a3f4e04bfa8e37f9f7002b89987
Wouter Termont <wouter.termont@ugent.be>
2024-03-24 14:10:11 +0100

    feat: add purpose claim
    Signed-off-by: Wouter Termont <wouter.termont@ugent.be>

--- packages/uma/src/credentials/Claims.ts
+++ packages/uma/src/credentials/Claims.ts
@@ -2,3 +2,4 @@
 export const WEBID = 'urn:solidlab:uma:claims:types:webid';
 export const CLIENTID = 'urn:solidlab:uma:claims:types:clientid';
 export const UNSOLVABLE = 'urn:solidlab:uma:claims:types:unsolvable';
+export const PURPOSE = 'http://www.w3.org/ns/odrl/2/purpose';
