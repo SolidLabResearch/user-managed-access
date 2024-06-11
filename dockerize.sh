f83fcb8f9d1a8613217b74231075486c9fcff02c
Ruben D. <ruben.dedecker@ugent.be>
2024-06-11 14:52:47 +0200

    fix: Added network=host flag to docker build

--- dockerize.sh
+++ dockerize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker image build --pull --file './Dockerfile' --tag 'solidtrustflows:latest' --label 'com.microsoft.created-by=visual-studio-code' ./
+docker image build --pull --file './Dockerfile' --tag 'solidtrustflows:latest' --label 'com.microsoft.created-by=visual-studio-code' --network=host ./ 
