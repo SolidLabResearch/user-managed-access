df9f9a0db2d0d3a44c6f1934fa35f08c6b7974b6
Joachim Van Herwegen <joachimvh@gmail.com>
2025-03-19 10:30:12 +0100

    feat: Simplify RoutedHttpRequestHandler

--- packages/uma/src/util/http/models/HttpHandlerController.ts
+++ packages/uma/src/util/http/models/HttpHandlerController.ts
@@ -1,13 +1,10 @@
-import { AsyncHandler } from '@solid/community-server';
 import { HttpHandlerContext } from './HttpHandlerContext';
 import { HttpHandlerRoute } from './HttpHandlerRoute';
 
 export class HttpHandlerController<C extends HttpHandlerContext = HttpHandlerContext> {
 
   constructor(
-    public label: string,
     public routes: HttpHandlerRoute<C>[],
-    public preResponseHandler?: AsyncHandler<HttpHandlerContext, C>,
   ) { }
 
 }
