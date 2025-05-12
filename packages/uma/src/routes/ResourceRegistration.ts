57505266df84b3258249394d6242306e67e3a390
Joachim Van Herwegen <joachimvh@gmail.com>
2025-05-12 14:31:57 +0200

    feat: Support PUT requests to update UMA registration

--- packages/uma/src/routes/ResourceRegistration.ts
+++ packages/uma/src/routes/ResourceRegistration.ts
@@ -1,3 +1,4 @@
+import { NotFoundHttpError } from '@solid/community-server';
 import {BadRequestHttpError} from '../util/http/errors/BadRequestHttpError';
 import {HttpHandler} from '../util/http/models/HttpHandler';
 import {HttpHandlerContext} from '../util/http/models/HttpHandlerContext';
@@ -49,12 +50,13 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
 
     switch (request.method) {
       case HttpMethods.POST: return this.handlePost(request);
+      case HttpMethods.PUT: return this.handlePut(request);
       case HttpMethods.DELETE: return this.handleDelete(request);
       default: throw new MethodNotAllowedHttpError();
     }
   }
 
-  private async handlePost(request: HttpHandlerRequest): Promise<HttpHandlerResponse<any>> {
+  private async handlePost(request: HttpHandlerRequest): Promise<HttpHandlerResponse> {
     const { headers, body } = request;
 
     if (headers['content-type'] !== 'application/json') {
@@ -69,7 +71,7 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
     }
 
     const resource = v4();
-    this.resourceStore.set(resource, body);
+    await this.resourceStore.set(resource, body);
 
     this.logger.info(`Registered resource ${resource}.`);
 
@@ -82,18 +84,50 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
         _id: resource,
         user_access_policy_uri: 'TODO: implement policy UI',
       }),
-    })
+    });
   }
 
-  private async handleDelete({ parameters }: HttpHandlerRequest): Promise<HttpHandlerResponse<any>> {
+  private async handlePut({ body, headers, parameters }: HttpHandlerRequest): Promise<HttpHandlerResponse> {
+    if (typeof parameters?.id !== 'string') throw new Error('URI for PUT operation should include an id.');
+
+    if (!await this.resourceStore.has(parameters.id)) {
+      throw new NotFoundHttpError();
+    }
+
+    if (headers['content-type'] !== 'application/json') {
+      throw new UnsupportedMediaTypeHttpError('Only Media Type "application/json" is supported for this route.');
+    }
+
+    try {
+      reType(body, ResourceDescription);
+    } catch (e) {
+      this.logger.warn('Syntax error: ' + (e as Error).message, body);
+      this.error(BadRequestHttpError, `Request has bad syntax${e instanceof Error ? ': ' + e.message : ''}`)
+    }
+
+    await this.resourceStore.set(parameters.id, body);
+    this.logger.info(`Updated resource ${parameters.id}.`);
+
+    return ({
+      status: 200,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        _id: parameters.id,
+        user_access_policy_uri: 'TODO: implement policy UI',
+      }),
+    });
+  }
+
+  private async handleDelete({ parameters }: HttpHandlerRequest): Promise<HttpHandlerResponse> {
     if (typeof parameters?.id !== 'string') throw new Error('URI for DELETE operation should include an id.');
 
     if (!await this.resourceStore.has(parameters.id)) {
-      throw new Error('Registration to be deleted does not exist (id unknown).');
+      throw new NotFoundHttpError();
     }
 
+    await this.resourceStore.delete(parameters.id);
     this.logger.info(`Deleted resource ${parameters.id}.`);
-    
+
     return ({
       status: 204,
       headers: {},
