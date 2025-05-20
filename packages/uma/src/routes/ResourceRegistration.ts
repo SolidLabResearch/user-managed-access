6e1a3ed0ebef04377572229c08c66319667ab618
Joachim Van Herwegen <joachimvh@gmail.com>
2025-05-20 13:57:48 +0200

    feat: Store collection metadata as triples when registering

--- packages/uma/src/routes/ResourceRegistration.ts
+++ packages/uma/src/routes/ResourceRegistration.ts
@@ -1,4 +1,7 @@
-import { ConflictHttpError, NotFoundHttpError } from '@solid/community-server';
+import { ConflictHttpError, NotFoundHttpError, RDF } from '@solid/community-server';
+import { ODRL, ODRL_P, UCRulesStorage } from '@solidlab/ucp';
+import { Store, DataFactory as DF, NamedNode, Quad_Subject } from 'n3';
+import { randomUUID } from 'node:crypto';
 import {BadRequestHttpError} from '../util/http/errors/BadRequestHttpError';
 import {HttpHandler} from '../util/http/models/HttpHandler';
 import {HttpHandlerContext} from '../util/http/models/HttpHandlerContext';
@@ -19,6 +22,11 @@ import { extractRequestSigner, verifyRequest } from '../util/HttpMessageSignatur
 type ErrorConstructor = { new(msg: string): Error };
 
 /**
+ * Relation between a collection source and one of its parts.
+ */
+type RelationEntry = { relation: NamedNode, source: NamedNode, part: NamedNode };
+
+/**
  * A ResourceRegistrationRequestHandler is tasked with implementing
  * section 3.2 from the User-Managed Access (UMA) Federated Auth 2.0.
  *
@@ -28,10 +36,12 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
   protected readonly logger: Logger = getLoggerFor(this);
 
   /**
-   * @param {RequestingPartyRegistration[]} resourceServers - Pod Servers to be registered with the UMA AS
+   * @param resourceStore - Key/value store containing the {@link ResourceDescription}s.
+   * @param policies - Policy store to contain the asset relation triples.
    */
   constructor(
     private readonly resourceStore: KeyValueStore<string, ResourceDescription>,
+    private readonly policies: UCRulesStorage,
   ) {}
 
   /**
@@ -75,7 +85,9 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
     let resource = body.name;
     if (resource) {
       if (await this.resourceStore.has(resource)) {
-        throw new ConflictHttpError(`${resource} is already registered. Use PUT to update existing registrations.`);
+        throw new ConflictHttpError(
+          `A resource with name ${resource} is already registered. Use PUT to update existing registrations.`,
+        );
       }
     } else {
       resource = v4();
@@ -84,6 +96,9 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
     await this.resourceStore.set(resource, body);
 
     this.logger.info(`Registered resource ${resource}.`);
+
+    // Store the new relations
+    await this.updateRelations(resource, body);
 
     return ({
       status: 201,
@@ -115,8 +130,13 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
       this.error(BadRequestHttpError, `Request has bad syntax${e instanceof Error ? ': ' + e.message : ''}`)
     }
 
+    // Keep track of the previous description so we know which relations need to change
+    const previousDescription = await this.resourceStore.get(parameters.id);
+
     await this.resourceStore.set(parameters.id, body);
     this.logger.info(`Updated resource ${parameters.id}.`);
+
+    await this.updateRelations(parameters.id, body, previousDescription);
 
     return ({
       status: 200,
@@ -142,6 +162,127 @@ export class ResourceRegistrationRequestHandler implements HttpHandler {
       status: 204,
       headers: {},
     });
+  }
+
+  /**
+   * Updates the stored relations in the policy storage based on the given input.
+   * @param id - UMA ID of the resource that changes.
+   * @param description - The new {@link ResourceDescription} for the resource.
+   * @param previous - The previous {@link ResourceDescription}, in case the resource was already registered previously.
+   */
+  protected async updateRelations(
+    id: string,
+    description: ResourceDescription,
+    previous?: ResourceDescription
+  ): Promise<void> {
+    const add: Record<string, RelationEntry> = this.toRelationEntries(id, description);
+    const remove: Record<string, RelationEntry> = this.toRelationEntries(id, previous);
+    this.filterRelationEntries(add, remove);
+
+    const policyStore = await this.policies.getStore();
+    await this.policies.addRule(this.entriesToStore(Object.values(add), policyStore));
+    await this.policies.removeData(this.entriesToStore(Object.values(remove), policyStore));
+  }
+
+  /**
+   * Creates a unique key based on the {@link RelationEntry} values.
+   */
+  protected getRelationKey(entry: RelationEntry): string {
+    return `${entry.source} ${entry.relation} ${entry.part}`;
+  }
+
+  /**
+   * Converts the `resource_relation` fields to {@link RelationEntry} objects.
+   * The keys of the object are generated with the `getRelationKey` function,
+   * resulting in a unique, but non-random, key for every value.
+   * @param id - UMA ID of the resource.
+   * @param description - {@link ResourceDescription} to analyze, if there is one.
+   */
+  protected toRelationEntries(id: string, description?: ResourceDescription): Record<string, RelationEntry> {
+    if (!description?.resource_relations) {
+      return {};
+    }
+
+    const result: Record<string, RelationEntry> = {};
+    for (let [ relation, targets ] of Object.entries(description.resource_relations)) {
+      if (!targets || targets.length === 0) {
+        continue;
+      }
+      const reverse = relation.startsWith('^');
+      if (reverse) {
+        relation = relation.slice(1);
+      }
+      const relationNode = DF.namedNode(relation);
+      for (const target of targets) {
+        const entry: RelationEntry = {
+          relation: relationNode,
+          part: DF.namedNode(reverse ? id : target),
+          source: DF.namedNode(reverse ? target : id),
+        };
+        result[this.getRelationKey(entry)] = entry;
+      }
+    }
+    return result;
+  }
+
+  /**
+   */
+  /**
+   * Removes entries that are present in both maps.
+   * These are the entries that remain unchanged.
+   * It is assumed that matching values have the same keys.
+   */
+  protected filterRelationEntries(
+    record1: Record<string, RelationEntry> = {},
+    record2: Record<string, RelationEntry> = {},
+  ): void {
+    for (const key of Object.keys(record1)) {
+      if (record2[key]) {
+        delete record1[key];
+        delete record2[key];
+      }
+    }
+  }
+
+  /**
+   * Converts the given entries into triples to add or remove to/from the policy store.
+   * @param entries - {@link RelationEntry} objects to parse.
+   * @param policyStore - {@link Store} with the relevant triples to update.
+   */
+  protected entriesToStore(entries: RelationEntry[], policyStore: Store): Store {
+    const store = new Store();
+    for (const entry of entries) {
+      const collectionIds = this.findCollectionIds(entry, policyStore);
+      if (collectionIds.length === 0) {
+        // TODO: if there is no collection we have to create one!
+        const collectionId = DF.namedNode(randomUUID());
+        store.addQuad(DF.quad(collectionId, RDF.terms.type, ODRL.terms.AssetCollection));
+        store.addQuad(DF.quad(collectionId, ODRL.terms.source, entry.source));
+        store.addQuad(DF.quad(collectionId, ODRL_P.terms.relation, entry.relation));
+        collectionIds.push(collectionId);
+        this.logger.info(`Creating new AssetCollection ${collectionId.value} with source ${
+          entry.source.value} and relation ${entry.relation.value}`);
+      }
+      // for (const collectionId of collectionIds) {
+      //   store.addQuad(DF.quad(entry.part, ODRL.terms.partOf, collectionId));
+      // }
+      // TODO: the above code is correct, but the code below is currently needed because of a bug in the ODRL evaluator
+      //       https://github.com/SolidLabResearch/ODRL-Evaluator/issues/8
+      store.addQuad(DF.quad(entry.part, ODRL.terms.partOf, entry.source));
+    }
+    return store;
+  }
+
+  /**
+   * Finds the identifiers of the collection(s) in the given {@link Store}
+   * that match the requirements of the given {@link RelationEntry}.
+   * @param entry - Relevant {@link RelationEntry}.
+   * @param data - {@link Store} in which to find the matching triples.
+   */
+  protected findCollectionIds(entry: RelationEntry, data: Store): Quad_Subject[] {
+    const sourceMatches = data.getSubjects(ODRL.terms.source, entry.source, null);
+    return sourceMatches.filter((subject): boolean =>
+      data.has(DF.quad(subject, ODRL_P.terms.relation, entry.relation)));
   }
 
   /**
