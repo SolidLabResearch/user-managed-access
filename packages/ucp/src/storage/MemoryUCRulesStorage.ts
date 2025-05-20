a193f2a7f70700cb801011ee2bdd51f1080b460d
Joachim Van Herwegen <joachimvh@gmail.com>
2025-05-20 13:56:42 +0200

    feat: Update UCRulesStorage interface to allow data removal

--- packages/ucp/src/storage/MemoryUCRulesStorage.ts
+++ packages/ucp/src/storage/MemoryUCRulesStorage.ts
@@ -1,16 +1,16 @@
-import { Store } from "n3";
-import { extractQuadsRecursive } from "../util/Util";
-import { UCRulesStorage } from "./UCRulesStorage";
+import { Store } from 'n3';
+import { extractQuadsRecursive } from '../util/Util';
+import { UCRulesStorage } from './UCRulesStorage';
 
 export class MemoryUCRulesStorage implements UCRulesStorage {
-    private store: Store;
+    protected store: Store;
 
     public constructor() {
         this.store = new Store();
     }
 
     public async getStore(): Promise<Store> {
-        return this.store;
+        return new Store(this.store);
     }
 
 
@@ -26,5 +26,9 @@ export class MemoryUCRulesStorage implements UCRulesStorage {
     public async deleteRule(identifier: string): Promise<void> {
         const store = await this.getRule(identifier)
         this.store.removeQuads(store.getQuads(null, null, null, null));
+    }
+
+    public async removeData(data: Store): Promise<void> {
+        this.store.removeQuads(data.getQuads(null, null, null, null));
     }
 }
