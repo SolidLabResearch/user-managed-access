b8a45d34c6fee6d4728cb7f10e9898b69bdc4cae
Joachim Van Herwegen <joachimvh@gmail.com>
2025-06-06 10:45:42 +0200

    refactor: Remove unused files

--- packages/ucp/src/util/Constants.ts
+++
@@ -1,3 +0,0 @@
-// TODO: maybe make enum
-export const ACCESS_MODES_ALLOWED = 'http://example.org/accessModesAllowed'
-export const ACCESS_MODES_PROHOBITID = 'http://example.org/accessModesProhobited'
