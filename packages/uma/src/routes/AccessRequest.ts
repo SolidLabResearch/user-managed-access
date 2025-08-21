8e3c044a3e199bc006ba461cafc8ba89f0a818cc
Bram Comyn <bram.comyn@ugent.be>
2025-08-21 15:34:59 +0200

    feat: initial (empty) implementation of the AccessRequestHandler

---
+++ packages/uma/src/routes/AccessRequest.ts
@@ -0,0 +1,43 @@
+import { BadRequestHttpError, getLoggerFor, MethodNotAllowedHttpError } from "@solid/community-server";
+import { HttpHandler , HttpHandlerContext, HttpHandlerRequest, HttpHandlerResponse} from "../util/http/models/HttpHandler";
+
+/**
+ * Endpoint to handle access requests
+ */
+export class AccessRequestHandler extends HttpHandler {
+
+    protected readonly logger = getLoggerFor(this);
+
+    constructor() {
+        super();
+    }
+
+    /**
+     * This function takes the GET-request with `Authorization: webID` and extracts the webID
+     * (To be altered with actual Solid-OIDC)
+     * 
+     * TODO: extract this from this file as well as from the [PublicRequestHandler](./Policy.ts).
+     * 
+     * @param request the request with the client 'id' as body
+     * @returns the client webID
+     */
+    protected getCredentials(request: HttpHandlerRequest): string {
+        const header = request.headers['authorization'];
+        if (typeof header !== 'string' && request.method !== "OPTIONS") {
+            throw new BadRequestHttpError('Missing Authorization header');
+        }
+        return header;
+    }
+
+    public async handle({ request }: HttpHandlerContext) : Promise<HttpHandlerResponse<any>> {
+        this.logger.info(`Received access request-grants request`)
+
+        switch (request.method) {
+            case 'GET': return new Promise(() => {});
+            case 'POST': return new Promise(() => {});
+            case 'PATCH': return new Promise(() => {});
+            default: throw new MethodNotAllowedHttpError();
+        }
+    }
+
+}
