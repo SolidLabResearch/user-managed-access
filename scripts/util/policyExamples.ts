be139a6798d8628645dedea3383d7a0c947c4181
Bram Comyn <bram.comyn@ugent.be>
2025-09-18 09:53:12 +0200

    fix: broken policy examples without uid

--- scripts/util/policyExamples.ts
+++ scripts/util/policyExamples.ts
@@ -5,6 +5,7 @@ export const policyA = `
 
 ex:usagePolicy1 a odrl:Agreement .
 ex:usagePolicy1 odrl:permission ex:permission1 .
+ex:usagePolicy1 odrl:uid ex:usagePolicy1 .
 ex:permission1 a odrl:Permission .
 ex:permission1 odrl:action odrl:modify .
 ex:permission1 odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -13,6 +14,7 @@ ex:permission1 odrl:assigner <https://pod.a.com/profile/card#me> .
 
 ex:usagePolicy1a a odrl:Agreement .
 ex:usagePolicy1a odrl:permission ex:permission1a .
+ex:usagePolicy1a odrl:uid ex:usagePolicy1a .
 ex:permission1a a odrl:Permission .
 ex:permission1a odrl:action odrl:read .
 ex:permission1a odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -21,6 +23,7 @@ ex:permission1a odrl:assigner <https://pod.a.com/profile/card#me> .
 
 <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> a odrl:Set;
     dct:description "A is data owner of resource X. ALICE may READ resource X.";
+    odrl:uid <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> ;
     odrl:permission <urn:uuid:f5199b0a-d824-45a0-bc08-1caa8d19a001> .
 <urn:uuid:f5199b0a-d824-45a0-bc08-1caa8d19a001> a odrl:Permission;
     odrl:action odrl:read;
@@ -34,6 +37,7 @@ export const policyB = `@prefix ex: <http://example.org/>.
 
 ex:usagePolicy2 a odrl:Agreement .
 ex:usagePolicy2 odrl:permission ex:permission2a .
+ex:usagePolicy2 odrl:uid ex:usagePolicy2 .
 ex:permission2 a odrl:Permission .
 ex:permission2 odrl:action odrl:modify .
 ex:permission2 odrl:target <http://localhost:3000/alice/other/> .
@@ -42,6 +46,7 @@ ex:permission2 odrl:assigner <https://pod.b.com/profile/card#me> .
 
 ex:usagePolicy2a a odrl:Agreement .
 ex:usagePolicy2a odrl:permission ex:permission2 .
+ex:usagePolicy2a odrl:uid ex:usagePolicy2a .
 ex:permission2a a odrl:Permission .
 ex:permission2a odrl:action odrl:write .
 ex:permission2a odrl:target <http://localhost:3000/alice/other/> .
@@ -55,6 +60,7 @@ export const putPolicyB = `
 @prefix dct: <http://purl.org/dc/terms/>.
 <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> a odrl:Set;
     dct:description "ZENO is data owner of resource X. ALICE may READ resource X.";
+    odrl:uid <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> ;
     odrl:permission <urn:uuid:69d57d36-74e5-443c-bae5-30159b0cbd3e> .
     
 <urn:uuid:69d57d36-74e5-443c-bae5-30159b0cbd3e> a odrl:Permission ;
@@ -69,6 +75,7 @@ export const policyC = `@prefix ex: <http://example.org/>.
 
 ex:usagePolicy3 a odrl:Agreement .
 ex:usagePolicy3 odrl:permission ex:permission3 .
+ex:usagePolicy3 odrl:uid ex:usagePolicy3 .
 ex:permission3 a odrl:Permission .
 ex:permission3 odrl:action odrl:read .
 ex:permission3 odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -82,6 +89,7 @@ export const badPolicy1 = `
 
 ex:usagePolicyBad a odrl:Agreement .
 ex:usagePolicyBad odrl:permission ex:permissionBad .
+ex:usagePolicyBad odrl:uid ex:usagePolicyBad .
 ex:permissionBad a odrl:Permission .
 ex:permissionBad odrl:action odrl:modify .
 ex:permissionBad odrl:target <http://localhost:3000/alice/other/> .
@@ -154,6 +162,7 @@ WHERE {
 export const putPolicy95e = `
 <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> a <http://www.w3.org/ns/odrl/2/Set>;
     <http://purl.org/dc/terms/description> "ZENO is data owner of resource X. ALICE may READ resource X.";
+    <http://www.w3.org/ns/odrl/2/uid> <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc>;
     <http://www.w3.org/ns/odrl/2/permission> <urn:uuid:a1111111-2222-3333-4444-555555555555>, <urn:uuid:b6666666-7777-8888-9999-aaaaaaaaaaaa>.
 <urn:uuid:a1111111-2222-3333-4444-555555555555> a <http://www.w3.org/ns/odrl/2/Permission>;
     <http://www.w3.org/ns/odrl/2/assignee> <http://example.org/alice>;
@@ -174,6 +183,7 @@ export const seedingPolicies = (id: string) => `
 
 ex:usagePolicy1 a odrl:Agreement .
 ex:usagePolicy1 odrl:permission ex:permission1 .
+ex:usagePolicy1 odrl:uid ex:usagePolicy1 .
 ex:permission1 a odrl:Permission .
 ex:permission1 odrl:action odrl:read .
 ex:permission1 odrl:action odrl:write .
@@ -184,6 +194,7 @@ ex:permission1 odrl:assigner <${id}> .
 
 ex:usagePolicy1a a odrl:Agreement .
 ex:usagePolicy1a odrl:permission ex:permission1a .
+ex:usagePolicy1a odrl:uid ex:usagePolicy1a .
 ex:permission1a a odrl:Permission .
 ex:permission1a odrl:action odrl:control .
 ex:permission1a odrl:target <http://localhost:3000/alice/other/otherFile.txt> .
@@ -193,6 +204,7 @@ ex:permission1a odrl:assigner <${id}> .
 
 <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> a odrl:Set;
     dct:description "A is data owner of resource X. ALICE may READ resource X.";
+    odrl:uid <urn:uuid:95efe0e8-4fb7-496d-8f3c-4d78c97829bc> ;
     odrl:permission <urn:uuid:f5199b0a-d824-45a0-bc08-1caa8d19a001> .
 <urn:uuid:f5199b0a-d824-45a0-bc08-1caa8d19a001> a odrl:Permission;
     odrl:action odrl:read;
@@ -204,6 +216,7 @@ ex:permission1a odrl:assigner <${id}> .
 
 ex:usagePolicy3 a odrl:Agreement .
 ex:usagePolicy3 odrl:permission ex:permission3 .
+ex:usagePolicy3 odrl:uid ex:usagePolicy3 .
 ex:permission3 a odrl:Permission .
 ex:permission3 odrl:action odrl:control .
 ex:permission3 odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -228,6 +241,7 @@ export const seedingPolicies2 = (id: string) => `
 
 ex:usagePolicy1-read a odrl:Agreement .
 ex:usagePolicy1-read odrl:permission ex:permission1-read .
+ex:usagePolicy1-read odrl:uid ex:usagePolicy1-read .
 ex:permission1-read a odrl:Permission .
 ex:permission1-read odrl:action odrl:read .
 ex:permission1-read odrl:target <http://localhost:3000/alice/other/oneFile.txt> .
@@ -236,6 +250,7 @@ ex:permission1-read odrl:assigner <${id}> .
 
 ex:usagePolicy1-write a odrl:Agreement .
 ex:usagePolicy1-write odrl:permission ex:permission1-write .
+ex:usagePolicy1-write odrl:uid ex:usagePolicy1-write .
 ex:permission1-write a odrl:Permission .
 ex:permission1-write odrl:action odrl:write .
 ex:permission1-write odrl:target <http://localhost:3000/alice/other/oneFile.txt> .
@@ -244,6 +259,7 @@ ex:permission1-write odrl:assigner <${id}> .
 
 ex:usagePolicy1-append a odrl:Agreement .
 ex:usagePolicy1-append odrl:permission ex:permission1-append .
+ex:usagePolicy1-append odrl:uid ex:usagePolicy1-append .
 ex:permission1-append a odrl:Permission .
 ex:permission1-append odrl:action odrl:append .
 ex:permission1-append odrl:target <http://localhost:3000/alice/other/oneFile.txt> .
@@ -252,6 +268,7 @@ ex:permission1-append odrl:assigner <${id}> .
 
 ex:usagePolicy1a-control-1 a odrl:Agreement .
 ex:usagePolicy1a-control-1 odrl:permission ex:permission1a-control-1 .
+ex:usagePolicy1a-control-1 odrl:uid ex:usagePolicy1a-control-1 .
 ex:permission1a-control-1 a odrl:Permission .
 ex:permission1a-control-1 odrl:action odrl:control .
 ex:permission1a-control-1 odrl:target <http://localhost:3000/alice/other/otherFile.txt> .
@@ -260,6 +277,7 @@ ex:permission1a-control-1 odrl:assigner <${id}> .
 
 ex:usagePolicy1a-control-2 a odrl:Agreement .
 ex:usagePolicy1a-control-2 odrl:permission ex:permission1a-control-2 .
+ex:usagePolicy1a-control-2 odrl:uid ex:usagePolicy1a-control-2 .
 ex:permission1a-control-2 a odrl:Permission .
 ex:permission1a-control-2 odrl:action odrl:control .
 ex:permission1a-control-2 odrl:target <http://localhost:3000/alice/other/otherFile.txt> .
@@ -268,6 +286,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
 
 <urn:uuid:policy-read> a odrl:Set;
     dct:description "A is data owner of resource X. ALICE may READ resource X.";
+    odrl:uid <urn:uuid:policy-read> ;
     odrl:permission <urn:uuid:perm-read> .
 <urn:uuid:perm-read> a odrl:Permission;
     odrl:action odrl:read;
@@ -276,6 +295,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
     odrl:assigner <${id}> .
 
 <urn:uuid:policy-append> a odrl:Set;
+    odrl:uid <urn:uuid:policy-append> ;
     odrl:permission <urn:uuid:perm-append> .
 <urn:uuid:perm-append> a odrl:Permission;
     odrl:action odrl:append;
@@ -284,6 +304,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
     odrl:assigner <${id}> .
 
 <urn:uuid:policy-write> a odrl:Set;
+    odrl:uid <urn:uuid:policy-write> ;
     odrl:permission <urn:uuid:perm-write> .
 <urn:uuid:perm-write> a odrl:Permission;
     odrl:action odrl:write;
@@ -293,6 +314,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
 
 ex:usagePolicy3-create a odrl:Agreement .
 ex:usagePolicy3-create odrl:permission ex:permission3-create .
+ex:usagePolicy3-create odrl:uid ex:usagePolicy3-create .
 ex:permission3-create a odrl:Permission .
 ex:permission3-create odrl:action odrl:create .
 ex:permission3-create odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -301,6 +323,7 @@ ex:permission3-create odrl:assigner <${id}> .
 
 ex:usagePolicy3b-create a odrl:Agreement .
 ex:usagePolicy3b-create odrl:permission ex:permission3b-create .
+ex:usagePolicy3b-create odrl:uid ex:usagePolicy3b-create .
 ex:permission3b-create a odrl:Permission .
 ex:permission3b-create odrl:action odrl:create .
 ex:permission3b-create odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -308,6 +331,7 @@ ex:permission3b-create odrl:assigner <${id}> .
 
 ex:usagePolicy3b-read a odrl:Agreement .
 ex:usagePolicy3b-read odrl:permission ex:permission3b-read .
+ex:usagePolicy3b-read odrl:uid ex:usagePolicy3b-read .
 ex:permission3b-read a odrl:Permission .
 ex:permission3b-read odrl:action odrl:read .
 ex:permission3b-read odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -315,6 +339,7 @@ ex:permission3b-read odrl:assigner <${id}> .
 
 ex:usagePolicy3b-write a odrl:Agreement .
 ex:usagePolicy3b-write odrl:permission ex:permission3b-write .
+ex:usagePolicy3b-write odrl:uid ex:usagePolicy3b-write .
 ex:permission3b-write a odrl:Permission .
 ex:permission3b-write odrl:action odrl:write .
 ex:permission3b-write odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -322,6 +347,7 @@ ex:permission3b-write odrl:assigner <${id}> .
 
 ex:usagePolicy3b-control a odrl:Agreement .
 ex:usagePolicy3b-control odrl:permission ex:permission3b-control .
+ex:usagePolicy3b-control odrl:uid ex:usagePolicy3b-control .
 ex:permission3b-control a odrl:Permission .
 ex:permission3b-control odrl:action odrl:control .
 ex:permission3b-control odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -334,20 +360,22 @@ export const seedingPolicies3 = (id: string) => `
 
 ex:usagePolicy1 a odrl:Agreement .
 ex:usagePolicy1 odrl:permission ex:permission1-read .
+ex:usagePolicy1 odrl:permission ex:permission1-write .
+ex:usagePolicy1 odrl:permission ex:permission1-append .
+ex:usagePolicy1 odrl:uid ex:usagePolicy1 .
+
 ex:permission1-read a odrl:Permission .
 ex:permission1-read odrl:action odrl:read .
 ex:permission1-read odrl:target <http://localhost:3000/alice/file.txt> .
 ex:permission1-read odrl:assignee <https://some.other.subject/profile/card#me> .
 ex:permission1-read odrl:assigner <${id}> .
 
-ex:usagePolicy1 odrl:permission ex:permission1-write .
 ex:permission1-write a odrl:Permission .
 ex:permission1-write odrl:action odrl:write .
 ex:permission1-write odrl:target <http://localhost:3000/alice/file.txt> .
 ex:permission1-write odrl:assignee <https://some.other.subject/profile/card#me> .
 ex:permission1-write odrl:assigner <${id}> .
 
-ex:usagePolicy1 odrl:permission ex:permission1-append .
 ex:permission1-append a odrl:Permission .
 ex:permission1-append odrl:action odrl:append .
 ex:permission1-append odrl:target <http://localhost:3000/alice/file.txt> .
@@ -356,13 +384,15 @@ ex:permission1-append odrl:assigner <${id}> .
 
 ex:usagePolicy1a a odrl:Agreement .
 ex:usagePolicy1a odrl:permission ex:permission1a-control-1 .
+ex:usagePolicy1a odrl:permission ex:permission1a-control-2 .
+ex:usagePolicy1a odrl:uid ex:usagePolicy1a .
+
 ex:permission1a-control-1 a odrl:Permission .
 ex:permission1a-control-1 odrl:action odrl:control .
 ex:permission1a-control-1 odrl:target <http://localhost:3000/alice/other/otherFile.txt> .
 ex:permission1a-control-1 odrl:assignee <https://some.other.subject/profile/card#me> .
 ex:permission1a-control-1 odrl:assigner <${id}> .
 
-ex:usagePolicy1a odrl:permission ex:permission1a-control-2 .
 ex:permission1a-control-2 a odrl:Permission .
 ex:permission1a-control-2 odrl:action odrl:control .
 ex:permission1a-control-2 odrl:target <http://localhost:3000/alice/other/otherFile.txt> .
@@ -371,6 +401,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
 
 <urn:uuid:policy-read> a odrl:Set;
     dct:description "A is data owner of resource X. ALICE may READ resource X.";
+    odrl:uid <urn:uuid:policy-read> ;
     odrl:permission <urn:uuid:perm-read> .
 <urn:uuid:perm-read> a odrl:Permission;
     odrl:action odrl:read;
@@ -379,6 +410,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
     odrl:assigner <${id}> .
 
 <urn:uuid:policy-append> a odrl:Set;
+    odrl:uid <urn:uuid:policy-append> ;
     odrl:permission <urn:uuid:perm-append> .
 <urn:uuid:perm-append> a odrl:Permission;
     odrl:action odrl:append;
@@ -387,6 +419,7 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
     odrl:assigner <${id}> .
 
 <urn:uuid:policy-write> a odrl:Set;
+    odrl:uid <urn:uuid:policy-write> ;
     odrl:permission <urn:uuid:perm-write> .
 <urn:uuid:perm-write> a odrl:Permission;
     odrl:action odrl:write;
@@ -396,32 +429,35 @@ ex:permission1a-control-2 odrl:assigner <${id}> .
 
 ex:usagePolicy3 a odrl:Agreement .
 ex:usagePolicy3 odrl:permission ex:permission3-create .
+ex:usagePolicy3 odrl:permission ex:permission3b-create .
+ex:usagePolicy3 odrl:permission ex:permission3b-read .
+ex:usagePolicy3 odrl:permission ex:permission3b-read .
+ex:usagePolicy3 odrl:permission ex:permission3b-write .
+ex:usagePolicy3 odrl:permission ex:permission3b-control .
+ex:usagePolicy3 odrl:uid ex:usagePolicy3 .
+
 ex:permission3-create a odrl:Permission .
 ex:permission3-create odrl:action odrl:write .
 ex:permission3-create odrl:target <http://localhost:3000/alice/other/resource.txt> .
 ex:permission3-create odrl:assignee <https://assignee/profile/card#me> .
 ex:permission3-create odrl:assigner <${id}> .
 
-ex:usagePolicy3 odrl:permission ex:permission3b-create .
 ex:permission3b-create a odrl:Permission .
 ex:permission3b-create odrl:action odrl:read .
 ex:permission3b-create odrl:target <http://localhost:3000/alice/other/resource.txt> .
 ex:permission3b-create odrl:assigner <${id}> .
 
-ex:usagePolicy3 odrl:permission ex:permission3b-read .
 ex:permission3b-read a odrl:Permission .
 ex:permission3b-read odrl:action odrl:read .
 ex:permission3b-read odrl:target <http://localhost:3000/alice/other/resource.txt> .
 ex:permission3b-read odrl:assigner <${id}> .
 
-ex:usagePolicy3 odrl:permission ex:permission3b-read .
 ex:permission3b-read a odrl:Permission .
 ex:permission3b-read odrl:action odrl:read .
 ex:permission3b-read odrl:target <http://localhost:3000/alice/other/resource.txt> .
 ex:permission3b-read odrl:assigner <${id}> .
 
 
-ex:usagePolicy3 odrl:permission ex:permission3b-write .
 ex:permission3b-write a odrl:Permission .
 ex:permission3b-write odrl:action odrl:write .
 ex:permission3b-write odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -429,7 +465,6 @@ ex:permission3b-write odrl:assigner <${id}> .
 ex:permission3b-write odrl:assignee <https://solidweb.me/Test-Pod-2/profile/card#me> .
 
 
-ex:usagePolicy3 odrl:permission ex:permission3b-control .
 ex:permission3b-control a odrl:Permission .
 ex:permission3b-control odrl:action odrl:control .
 ex:permission3b-control odrl:target <http://localhost:3000/alice/other/resource.txt> .
@@ -444,6 +479,7 @@ export const seedTestFetcher = `
 
 ex:usagePolicy1 a odrl:Agreement .
 ex:usagePolicy1 odrl:permission ex:permission1 .
+ex:usagePolicy1 odrl:uid ex:usagePolicy1 .
 ex:permission1 a odrl:Permission .
 ex:permission1 odrl:action odrl:modify .
 ex:permission1 odrl:target <alice/other/resource.txt> .
@@ -452,6 +488,7 @@ ex:permission1 odrl:assigner <https://pod.woutslabbinck.com/profile/card#me> .
 
 ex:usagePolicy1a a odrl:Agreement .
 ex:usagePolicy1a odrl:permission ex:permission1a .
+ex:usagePolicy1a odrl:uid ex:usagePolicy1a .
 ex:permission1a a odrl:Permission .
 ex:permission1a odrl:action odrl:create .
 ex:permission1a odrl:target <alice/other/resource.txt> .
@@ -460,6 +497,7 @@ ex:permission1a odrl:assigner <https://pod.woutslabbinck.com/profile/card#me> .
 
 ex:usagePolicy2 a odrl:Agreement .
 ex:usagePolicy2 odrl:permission ex:permission2a .
+ex:usagePolicy2 odrl:uid ex:usagePolicy2 .
 ex:permission2 a odrl:Permission .
 ex:permission2 odrl:action odrl:modify .
 ex:permission2 odrl:target <alice/other/> .
@@ -468,6 +506,7 @@ ex:permission2 odrl:assigner <https://pod.woutslabbinck.com/profile/card#me> .
 
 ex:usagePolicy2a a odrl:Agreement .
 ex:usagePolicy2a odrl:permission ex:permission2 .
+ex:usagePolicy2a odrl:uid ex:usagePolicy2a .
 ex:permission2a a odrl:Permission .
 ex:permission2a odrl:action odrl:create .
 ex:permission2a odrl:target <alice/other/> .
@@ -477,6 +516,7 @@ ex:permission2a odrl:assigner <https://pod.woutslabbinck.com/profile/card#me> .
 
 ex:usagePolicy3 a odrl:Agreement .
 ex:usagePolicy3 odrl:permission ex:permission3 .
+ex:usagePolicy3 odrl:uid ex:usagePolicy3 .
 ex:permission3 a odrl:Permission .
 ex:permission3 odrl:action odrl:read .
 ex:permission3 odrl:target <alice/other/resource.txt> .
