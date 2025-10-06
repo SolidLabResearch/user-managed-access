6fb6b6a1dcdab6c9f17a3573b7667b8715b51c9d
Joachim Van Herwegen <joachimvh@gmail.com>
2025-10-06 11:02:10 +0200

    feat: Support meta credential tokens

--- packages/uma/src/credentials/Formats.ts
+++ packages/uma/src/credentials/Formats.ts
@@ -1,4 +1,5 @@
 
 export const JWT = 'urn:solidlab:uma:claims:formats:jwt';
 export const UNSECURE = 'urn:solidlab:uma:claims:formats:webid';
+export const META = 'urn:solidlab:uma:claims:formats:meta';
 export const OIDC = 'http://openid.net/specs/openid-connect-core-1_0.html#IDToken';
--- packages/uma/src/credentials/verify/TypedVerifier.ts
+++ packages/uma/src/credentials/verify/TypedVerifier.ts
@@ -3,12 +3,32 @@ import { ClaimSet } from "../ClaimSet";
 import { Credential } from "../Credential";
 import { Verifier } from "./Verifier";
 
+/**
+ * Redirect verification requests to the relevant verifier by matching the credential format
+ * to the keys of the internal verifier map.
+ * In case the credential format is the meta type, it is assumed the value is a key/value map
+ * with keys being credential formats, and values the associated tokens.
+ * These results will be merged, if multiple credentials map to the same claim,
+ * the result will depend on the execution order.
+ */
 export class TypedVerifier implements Verifier {
   private readonly logger = getLoggerFor(this);
 
-  constructor(protected verifiers: Record<string, Verifier>) {}
+  constructor(
+    protected readonly verifiers: Record<string, Verifier>,
+    protected readonly metaType?: string,
+  ) {}
 
   public async verify(credential: Credential): Promise<ClaimSet> {
+    // Recursively verify in case of a meta credential token
+    if (credential.format === this.metaType) {
+      const metaClaims = {};
+      for (const [ format, token ] of Object.entries(credential.token)) {
+        Object.assign(metaClaims, await this.verify({ format, token }));
+      }
+      return metaClaims;
+    }
+
     const verifier = this.verifiers[credential.format];
     this.logger.debug(`Verifying credential with typed verifier ${JSON.stringify(credential)}`);
 
