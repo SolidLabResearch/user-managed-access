9251bdfbcdbf28196f3b84624aa11cc26fb8a8d2
maartenvandenbrande <maarten.vdbrande@gmail.com>
2026-01-30 10:00:01 +0100

    feat: cleanup defaults + add backupFilePath option

--- packages/uma/bin/main.js
+++ packages/uma/bin/main.js
@@ -15,6 +15,7 @@ const argv = yargs(hideBin(process.argv))
     alias: 'b',
     type: 'string',
     description: 'Base URL for the UMA server',
+    default: `http://localhost:${argv.port}/uma`
   })
   .option('loggingLevel', {
     alias: 'l',
@@ -22,32 +23,33 @@ const argv = yargs(hideBin(process.argv))
     description: 'Log level for the UMA server',
     default: 'info'
   })
+  .option('backupFilePath', {
+    alias: 'f',
+    type: 'string',
+    description: 'Backup file path for the UMA server',
+    default: ''
+  })
   .help()
   .alias('help', 'h')
   .argv;
 
-const port = argv.port;
-const baseUrl = argv.baseUrl || `http://localhost:${port}/uma`;
 const rootDir = path.join(__dirname, '../');
 
 const launch = async () => {
   const variables = {};
 
-  variables['urn:uma:variables:port'] = port;
-  variables['urn:uma:variables:baseUrl'] = baseUrl;
-
-  variables['urn:uma:variables:policyBaseIRI'] = 'http://localhost:3000/';
-  variables['urn:uma:variables:policyDir'] = path.join(rootDir, './config/rules/policy');
+  variables['urn:uma:variables:port'] = argv.port;
+  variables['urn:uma:variables:baseUrl'] = argv.baseUrl;
   variables['urn:uma:variables:eyePath'] = 'eye';
-  variables['urn:uma:variables:backupFilePath'] = 'backup.ttl';
+  variables['urn:uma:variables:backupFilePath'] = argv.backupFilePath;
 
   const configPath = path.join(rootDir, './config/default.json');
 
-  setGlobalLoggerFactory(new WinstonLoggerFactory(argv.logLevel || 'info'));
+  setGlobalLoggerFactory(new WinstonLoggerFactory(argv.loggingLevel));
 
   const manager = await ComponentsManager.build({
     mainModulePath: rootDir,
-    logLevel: argv.logLevel || 'silly',
+    logLevel: argv.loggingLevel,
     typeChecking: false,
   });
 
